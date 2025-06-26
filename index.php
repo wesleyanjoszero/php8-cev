2d8876890a2cf78c5c9752927bb007c721d1ed69
WESLEY ANJOS <wesleyanjoszero@gmail.com>
2025-06-26 15:45:11 -0300

    adicionando estrutura html

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
-<?php
-
-phpinfo();
-
-?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>me livrei da maldição</title>
+</head>
+<body>
+    <h1>
+        <?php
+            echo 'Hello, world!';
+        ?>
+    </h1>
+    <p>Vamos tentar nos livrar da maldição</p>
+</body>
+</html>
