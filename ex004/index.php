7284162637cf6f253341fafb28319e35933139e9
WESLEY ANJOS <wesleyanjoszero@gmail.com>
2025-06-28 11:57:05 -0300

    add ex004 string

---
+++ ex004/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>String</title>
+</head>
+<body>
+    <?php
+        $name = 'Wesley';
+        $lastName = 'Anjos';
+        echo "Seu nome e sobrenome é $name $lastName \u{1F596}<br>";
+    ?>
+</body>
+</html>
